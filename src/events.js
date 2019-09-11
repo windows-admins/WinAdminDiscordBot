00f069457a08a0d044f92971f68eda2b74fa5fba
Riley Childs (MCS) <richilds@microsoft.com>
2019-09-10 23:41:46 -0700

    updated help

--- src/events.js
+++ src/events.js
@@ -177,10 +177,10 @@ const sendHelp = ( event ) => {
     '• `@Someone--`: Subtract points from a user or a thing\n' +
     '• `@Someone==`: Gets current points from a user or a thing\n' +
     // '• `@Someone##`: Randomly adds or removes 1-5 points from a user or a thing\n' +
-    '• `<@' + botUserID + '> leaderboard`: Display the leaderboard for just you\n' +
-    '• `<@' + botUserID + '> leaderboardall ' + shasum.digest('hex') + '`: Display the leaderboard for everyone (you need your secret key)\n' +
-    '• `<@' + botUserID + '> help`: Display this message just for you\n\n' +
-    '• `<@' + botUserID + '> helpall`: Display this message for everyone\n\n' +
+    '• `<@621233702235537408> leaderboard`: Display the leaderboard for just you\n' +
+    '• `<@621233702235537408> leaderboardall ' + shasum.digest('hex') + '`: Display the leaderboard for everyone (you need your secret key)\n' +
+    '• `<@621233702235537408> help`: Display this message just for you\n\n' +
+    '• `<@621233702235537408> helpall`: Display this message for everyone\n\n' +
     'You\'ll need to invite me to a channel before I can recognise ' +
     '`++` and `--` commands in it.\n\n' +
     'If you\'re a developer, you can teach me new things! :awwww_yeah:\n\n  ' 
@@ -202,17 +202,17 @@ const sendHelp = ( event ) => {
  */
 const sendAllHelp = ( event ) => {
 
-  const botUserID = helpers.extractUserID( event.text );
+ 
   const message = (
     'Sure, here\'s what I can do:\n\n' +
     '• `@Someone++`: Add points to a user or a thing\n' +
     '• `@Someone--`: Subtract points from a user or a thing\n' +
     '• `@Someone==`: Gets current points from a user or a thing\n' +
     // '• `@Someone##`: Randomly adds or removes 1-5 points from a user or a thing\n' +
-    '• `<@' + botUserID + '> leaderboard`: Display the leaderboard for just you\n' +
-    '• `<@' + botUserID + '> leaderboardall {your secret key from help}`: Display the leaderboard for everyone (you need your secret key)\n' +
-    '• `<@' + botUserID + '> help`: Display this message just for you\n\n' +
-    '• `<@' + botUserID + '> helpall`: Display this message for everyone\n\n' +
+    '• `<@621233702235537408> leaderboard`: Display the leaderboard for just you\n' +
+    '• `<@621233702235537408> leaderboardall {your secret key from help}`: Display the leaderboard for everyone (you need your secret key)\n' +
+    '• `<@621233702235537408> help`: Display this message just for you\n\n' +
+    '• `<@621233702235537408> helpall`: Display this message for everyone\n\n' +
     'You\'ll need to invite me to a channel before I can recognise ' +
     '`++` and `--` commands in it.\n\n' +
     'If you\'re a developer, you can teach me new things! :awwww_yeah:\n\n  ' 
